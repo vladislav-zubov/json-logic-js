a801db105b5baaebf56e81e1d1bb0e8d27420e8e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-01-23 16:43:37 +0100

    fix: lint errors

--- rollup.config.js
+++ rollup.config.js
@@ -4,34 +4,33 @@ const uglify = require('rollup-plugin-uglify');
 const optimize = require('rollup-plugin-optimize-js');
 const filesize = require('rollup-plugin-filesize');
 
-module.exports = [{
-  input: 'src/index.js',
-  plugins: [
-    resolve(),
-    babel(),
-    filesize({ showMinifiedSize: false }),
-  ],
-  output: {
-    file: 'dist/jsonLogic.js',
-    format: 'umd',
-    name: 'jsonLogic',
-    exports: 'default',
-    sourcemap: true,
-  }
-}, {
-  input: 'src/index.js',
-  plugins: [
-    resolve(),
-    babel(),
-    uglify.uglify(),
-    optimize(),
-    filesize({ showMinifiedSize: false }),
-  ],
-  output: {
-    file: 'dist/jsonLogic.min.js',
-    format: 'umd',
-    name: 'jsonLogic',
-    exports: 'default',
-    sourcemap: true,
-  }
-}];
+module.exports = [
+  {
+    input: 'src/index.js',
+    plugins: [resolve(), babel(), filesize({ showMinifiedSize: false })],
+    output: {
+      file: 'dist/jsonLogic.js',
+      format: 'umd',
+      name: 'jsonLogic',
+      exports: 'default',
+      sourcemap: true,
+    },
+  },
+  {
+    input: 'src/index.js',
+    plugins: [
+      resolve(),
+      babel(),
+      uglify.uglify(),
+      optimize(),
+      filesize({ showMinifiedSize: false }),
+    ],
+    output: {
+      file: 'dist/jsonLogic.min.js',
+      format: 'umd',
+      name: 'jsonLogic',
+      exports: 'default',
+      sourcemap: true,
+    },
+  },
+];
